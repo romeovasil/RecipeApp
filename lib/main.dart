d61fb1ad0600538eb77bb75ee641e9fb242f152d
romeovasil <romeo.vasil@irian.ro>
2023-12-17 22:59:28 +0200

    firebase config changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:chs_p/features/app/splash_screen/splash_screen.dart';
 import 'package:chs_p/features/user_auth/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget{
